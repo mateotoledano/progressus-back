7eb0305055cfa45013d349fdfcfab58a4d72b159
PabloAimar <67346571+PabloA128@users.noreply.github.com>
2024-12-05 17:57:00 -0300

    Se agregan endpoints abm merch

---
+++ ProgressusWebApi/Services/MerchServices/Interfaces/IMerchService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgressusWebApi.Dtos.MerchDtos;
+
+namespace ProgressusWebApi.Services.MerchServices.Interfaces
+{
+    public interface IMerchService
+    {
+        // Crear un nuevo merch
+        Task<IActionResult> CrearMerchAsync(MerchDtos merchDto);
+
+        // Obtener todos los merch
+        Task<IActionResult> ObtenerTodosMerchAsync();
+
+        // Obtener un merch por su Id
+        Task<IActionResult> ObtenerMerchPorIdAsync(string id);
+
+        // Editar un merch existente
+        Task<IActionResult> EditarMerchAsync(string id, MerchDtos merchDto);
+
+        // Eliminar un merch
+        Task<IActionResult> EliminarMerchAsync(string id);
+    }
+}
