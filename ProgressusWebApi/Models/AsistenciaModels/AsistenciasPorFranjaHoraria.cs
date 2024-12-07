55e4416314568340ad7e8e2e3297b70e2c411310
Mateo Toledano <mateo.toledano@gmail.com>
2024-12-07 15:53:09 -0300

    add: cambios franc

---
+++ ProgressusWebApi/Models/AsistenciaModels/AsistenciasPorFranjaHoraria.cs
@@ -0,0 +1,9 @@
+﻿namespace ProgressusWebApi.Models.AsistenciaModels
+{
+    public class AsistenciasPorFranjaHoraria
+    {
+        public int FranjaHoraria { get; set; } // Cambiado a int
+        public int NumeroDeAsistencias { get; set; }
+    }
+
+}
