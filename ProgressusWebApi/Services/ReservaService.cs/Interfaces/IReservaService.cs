a5effab657e83c7afb58223fdb54f3fade282c71
igna_martinoli <ignamartinoli@protonmail.com>
2025-02-26 21:22:06 -0300

    Revert "agregado lo de pablo"
    This reverts commit ba45d458a9484833143a2a6d4f57b123feed3c28.

--- ProgressusWebApi/Services/ReservaService.cs/Interfaces/IReservaService.cs
+++ ProgressusWebApi/Services/ReservaService.cs/Interfaces/IReservaService.cs
@@ -21,7 +21,6 @@ namespace ProgressusWebApi.Services.ReservaService.cs.interfaces
 
         // Método para registrar la asistencia de un usuario
         Task<bool> RegistrarAsistenciaAsync(string userId);
-        Task<bool> IngresoConClaveAsync(string userId, string clave);
 
         // Método para registrar el log de asistencia
         Task RegistrarLogDeAsistenciaAsync(string userId, int reservaId);
@@ -38,7 +37,8 @@ namespace ProgressusWebApi.Services.ReservaService.cs.interfaces
         Task<List<AsistenciasPorMesYDia>> ObtenerAsistenciasPorMesYDiaAsync();
 
         Task<List<AsistenciasPorMes>> ObtenerAsistenciasPorMesAsync();
-        
+
+        Task<bool> TodasLasReservasSonAntiguas(string userId);
 
 
         }
