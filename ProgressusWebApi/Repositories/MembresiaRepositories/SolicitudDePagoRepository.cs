47139a16453f1303ab53340b65902d313b3fe13e
igna_martinoli <ignamartinoli@protonmail.com>
2025-04-09 19:38:47 -0300

    sacado el membresiaid != 15 de ambos metodos correctos

--- ProgressusWebApi/Repositories/MembresiaRepositories/SolicitudDePagoRepository.cs
+++ ProgressusWebApi/Repositories/MembresiaRepositories/SolicitudDePagoRepository.cs
@@ -14,7 +14,10 @@ namespace ProgressusWebApi.Repositories.MembresiaRepositories
         private readonly ProgressusDataContext _context;
         private readonly IMembresiaRepository _membresiaRepository;
 
-        public SolicitudDePagoRepository(ProgressusDataContext context, IMembresiaRepository membresiaRepository)
+        public SolicitudDePagoRepository(
+            ProgressusDataContext context,
+            IMembresiaRepository membresiaRepository
+        )
         {
             _context = context;
             _membresiaRepository = membresiaRepository;
@@ -34,21 +37,25 @@ namespace ProgressusWebApi.Repositories.MembresiaRepositories
             await _context.SaveChangesAsync();
             return solicitud;
         }
-        public async Task<List<SolicitudDePago>> ObtenerSolicitudesDePagoDeSocio(string identityUserId)
+
+        public async Task<List<SolicitudDePago>> ObtenerSolicitudesDePagoDeSocio(
+            string identityUserId
+        )
         {
-            return await _context.SolicitudDePagos
-        .Where(s => s.IdentityUserId == identityUserId && s.MembresiaId != 15) // Filtrar por IdentityUserId y excluir MembresiaId = 15
+            return await _context
+                .SolicitudDePagos.Where(s => s.IdentityUserId == identityUserId) // Filtrar por IdentityUserId y excluir MembresiaId = 15
                 .Include(s => s.Membresia) // Incluir la membresía
                 .Include(s => s.TipoDePago) // Incluir el tipo de pago
                 .Include(s => s.HistorialSolicitudDePagos) // Incluir el historial
-                    .ThenInclude(h => h.EstadoSolicitud) // Incluir el estado de solicitud dentro del historial
+                .ThenInclude(h => h.EstadoSolicitud) // Incluir el estado de solicitud dentro del historial
                 .OrderByDescending(s => s.FechaCreacion) // Ordenar por fecha de creación (más reciente primero)
                 .ToListAsync();
         }
+
         public async Task<SolicitudDePago> ObtenerSolicitudDePagoPorIdAsync(int solicitudId)
         {
-            return await _context.SolicitudDePagos
-                .Include(s => s.TipoDePago)
+            return await _context
+                .SolicitudDePagos.Include(s => s.TipoDePago)
                 .Include(s => s.Membresia)
                 .Include(s => s.HistorialSolicitudDePagos)
                 .FirstOrDefaultAsync(s => s.Id == solicitudId);
@@ -59,7 +66,6 @@ namespace ProgressusWebApi.Repositories.MembresiaRepositories
         {
             return await _context.TipoDePagos.FirstOrDefaultAsync(tp => tp.Nombre == nombre);
         }
-
 
         // Métodos para EstadoSolicitud
         public async Task<EstadoSolicitud> ObtenerEstadoSolicitudPorNombreAsync(string nombre)
@@ -73,25 +79,31 @@ namespace ProgressusWebApi.Repositories.MembresiaRepositories
         }
 
         // Métodos para HistorialSolicitudDePago
-        public async Task<HistorialSolicitudDePago> CrearHistorialSolicitudDePagoAsync(HistorialSolicitudDePago historial)
+        public async Task<HistorialSolicitudDePago> CrearHistorialSolicitudDePagoAsync(
+            HistorialSolicitudDePago historial
+        )
         {
             await _context.HistorialSolicitudDePagos.AddAsync(historial);
             await _context.SaveChangesAsync();
             return historial;
         }
 
-        public async Task<IEnumerable<HistorialSolicitudDePago>> ObtenerTodoElHistorialDeUnaSolicitudAsync(int solicitudId)
+        public async Task<
+            IEnumerable<HistorialSolicitudDePago>
+        > ObtenerTodoElHistorialDeUnaSolicitudAsync(int solicitudId)
         {
-            return await _context.HistorialSolicitudDePagos
-                .Where(h => h.SolicitudDePagoId == solicitudId)
+            return await _context
+                .HistorialSolicitudDePagos.Where(h => h.SolicitudDePagoId == solicitudId)
                 .OrderBy(h => h.FechaCambioEstado)
                 .ToListAsync();
         }
 
-        public async Task<HistorialSolicitudDePago> ObtenerUltimoHistorialDeUnaSolicitudAsync(int solicitudId)
+        public async Task<HistorialSolicitudDePago> ObtenerUltimoHistorialDeUnaSolicitudAsync(
+            int solicitudId
+        )
         {
-            HistorialSolicitudDePago historialSolicitudDePago = await _context.HistorialSolicitudDePagos
-                .Where(h => h.SolicitudDePagoId == solicitudId)
+            HistorialSolicitudDePago historialSolicitudDePago = await _context
+                .HistorialSolicitudDePagos.Where(h => h.SolicitudDePagoId == solicitudId)
                 .OrderByDescending(h => h.FechaCambioEstado)
                 .FirstOrDefaultAsync();
             return (historialSolicitudDePago);
@@ -99,31 +111,31 @@ namespace ProgressusWebApi.Repositories.MembresiaRepositories
 
         public Task<TipoDePago> ObtenerTipoDePagoPorIdAsync(int id)
         {
-            return  _context.TipoDePagos.FirstOrDefaultAsync(tp => tp.Id == id);
+            return _context.TipoDePagos.FirstOrDefaultAsync(tp => tp.Id == id);
         }
 
-     public async Task<SolicitudDePago> ObtenerSolicitudDePagoDeSocio(string identityUserId)
-{
-    // Obtener la solicitud de pago más reciente del socio, excluyendo las que tienen MembresiaId = 15
-    SolicitudDePago? solicitud = await _context.SolicitudDePagos
-        .Where(s => s.IdentityUserId == identityUserId && s.MembresiaId != 15) // Filtrar por IdentityUserId y excluir MembresiaId = 15
-        .Include(s => s.HistorialSolicitudDePagos) // Incluye el historial completo
-        .ThenInclude(h => h.EstadoSolicitud)       // Incluye también el EstadoSolicitud de cada historial
-        .OrderByDescending(s => s.FechaCreacion)   // Ordenar por fecha de creación descendente
-        .FirstOrDefaultAsync();                    // Obtener la primera coincidencia
+        public async Task<SolicitudDePago> ObtenerSolicitudDePagoDeSocio(string identityUserId)
+        {
+            // Obtener la solicitud de pago más reciente del socio, excluyendo las que tienen MembresiaId = 15
+            SolicitudDePago? solicitud = await _context
+                .SolicitudDePagos.Where(s => s.IdentityUserId == identityUserId) // Filtrar por IdentityUserId y excluir MembresiaId = 15
+                .Include(s => s.HistorialSolicitudDePagos) // Incluye el historial completo
+                .ThenInclude(h => h.EstadoSolicitud) // Incluye también el EstadoSolicitud de cada historial
+                .OrderByDescending(s => s.FechaCreacion) // Ordenar por fecha de creación descendente
+                .FirstOrDefaultAsync(); // Obtener la primera coincidencia
 
-    // Si no se encuentra ninguna solicitud, devolver null
-    if (solicitud == null)
-    {
-        return null;
-    }
+            // Si no se encuentra ninguna solicitud, devolver null
+            if (solicitud == null)
+            {
+                return null;
+            }
 
-    // Cargar el TipoDePago y la Membresia de manera asíncrona
-    solicitud.TipoDePago = await this.ObtenerTipoDePagoPorIdAsync(solicitud.TipoDePagoId);
-    solicitud.Membresia = await _membresiaRepository.GetById(solicitud.MembresiaId);
+            // Cargar el TipoDePago y la Membresia de manera asíncrona
+            solicitud.TipoDePago = await this.ObtenerTipoDePagoPorIdAsync(solicitud.TipoDePagoId);
+            solicitud.Membresia = await _membresiaRepository.GetById(solicitud.MembresiaId);
 
-    return solicitud;
-}
+            return solicitud;
+        }
 
         public async Task<IActionResult> ObtenerTiposDePagos()
         {
@@ -133,20 +145,24 @@ namespace ProgressusWebApi.Repositories.MembresiaRepositories
 
         public async Task<IActionResult> ConsultarVigenciaDeMembresia(string userId)
         {
-            var solicitud = await _context.SolicitudDePagos
-                .Where(s => s.IdentityUserId == userId.ToString())
+            var solicitud = await _context
+                .SolicitudDePagos.Where(s => s.IdentityUserId == userId.ToString())
                 .Include(s => s.HistorialSolicitudDePagos) // Incluye el historial completo
-                    .ThenInclude(h => h.EstadoSolicitud)    // Incluye también el EstadoSolicitud de cada historial
-                .OrderByDescending(s => s.HistorialSolicitudDePagos
-                    .Where(h => h.EstadoSolicitud.Nombre == "Confirmado")
-                    .OrderByDescending(h => h.FechaCambioEstado)
-                    .FirstOrDefault().FechaCambioEstado)
+                .ThenInclude(h => h.EstadoSolicitud) // Incluye también el EstadoSolicitud de cada historial
+                .OrderByDescending(s =>
+                    s.HistorialSolicitudDePagos.Where(h => h.EstadoSolicitud.Nombre == "Confirmado")
+                        .OrderByDescending(h => h.FechaCambioEstado)
+                        .FirstOrDefault()
+                        .FechaCambioEstado
+                )
                 .FirstOrDefaultAsync();
 
             // Verifica si no se encontró una solicitud con estado "Confirmado"
             if (solicitud == null)
             {
-                return new NotFoundObjectResult("No se encontró ninguna solicitud confirmada para el usuario.");
+                return new NotFoundObjectResult(
+                    "No se encontró ninguna solicitud confirmada para el usuario."
+                );
             }
 
             var historial = await ObtenerUltimoHistorialDeUnaSolicitudAsync(solicitud.Id);
@@ -165,20 +181,23 @@ namespace ProgressusWebApi.Repositories.MembresiaRepositories
             {
                 VigenteDesde = fechaConfirmacion,
                 VigenteHasta = fechaVigencia,
-                EsVigente = fechaVigencia >= DateTime.Now
+                EsVigente = fechaVigencia >= DateTime.Now,
             };
 
             return new OkObjectResult(vigenciaDto);
         }
 
-    
-    public async Task<IActionResult> ObtenerTodasLasSolicitudesDeUnSocio(string identityUserId)
+        public async Task<IActionResult> ObtenerTodasLasSolicitudesDeUnSocio(string identityUserId)
         {
-            var solicitudes = await _context.SolicitudDePagos
-                .Where(s => s.IdentityUserId == identityUserId.ToString() &&
-                            s.HistorialSolicitudDePagos.Any(h => h.EstadoSolicitud.Nombre == "Confirmado")) // Filtra por historial con estado "Confirmado"
+            var solicitudes = await _context
+                .SolicitudDePagos.Where(s =>
+                    s.IdentityUserId == identityUserId.ToString()
+                    && s.HistorialSolicitudDePagos.Any(h =>
+                        h.EstadoSolicitud.Nombre == "Confirmado"
+                    )
+                ) // Filtra por historial con estado "Confirmado"
                 .Include(s => s.HistorialSolicitudDePagos) // Incluye el historial completo
-                    .ThenInclude(h => h.EstadoSolicitud)
+                .ThenInclude(h => h.EstadoSolicitud)
                 .Include(s => s.TipoDePago)
                 .Include(s => s.Membresia)
                 .OrderByDescending(s => s.FechaCreacion) // Ordena por fecha de creación de la solicitud
